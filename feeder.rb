e17b16f5c014885f38dbc63305b5a421297b52d8
Autumn _ <orzumautumn@gmail.com>
2012-06-01 14:11:01 +1000

    Weird error with TT feeds, fixed.

--- feeder.rb
+++ feeder.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 # feed = Nokogiri::HTML(open("http://tokyotosho.info/rss.php?filter=1"))
 
 def open_feed(url)
-    feed = open("http://tokyotosho.info/rss.php?filter=1").read
+    feed = open(url).read
     feed = feed.gsub(/<link>/, "<url>")
     feed = feed.gsub(/<\/link>/, "</url>")
     feed = feed.gsub(/\]\]>/, "")
@@ -24,6 +24,8 @@ def watching(title)
         elsif /Haiyore! Nyaruko-san/ =~ title
             watching = true
         end
+    elsif /\[Over-Time\]/ =~ title and /Akibaranger/ =~ title and /SD/ !~ title
+        watching = true
     elsif /\[Mazui\]/ =~ title and /Hyouka/ =~ title
         watching = true
     elsif /\[EveTaku\]/ =~ title and /AKB0048/ =~ title
@@ -50,17 +52,24 @@ def parse_item(item)
 
 end
 
-feed = open_feed("http://tokyotosho.info/rss.php?filter=1")
-
-items = feed.xpath("//item")
-
-puts "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-puts "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">"
-puts "<channel><atom:link href=\"http://tokyotosho.info/rss.php\" rel=\"self\" type=\"application/rss+xml\" />"
-puts "<title>Haruchin sugoi! =w=</title>"
-puts
-
-items.each do |item|
-    parse_item(Nokogiri::XML(item.to_s))
+def begin_feed()
+    puts "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    puts "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">"
+    puts "<channel><atom:link href=\"http://tokyotosho.info/rss.php\" rel=\"self\" type=\"application/rss+xml\" />"
+    puts "<title>Haruchin sugoi! =w=</title>"
+    puts
 end
 
+def print_feed(url)
+    feed = open_feed(url)
+
+    items = feed.xpath("//item")
+
+    items.each do |item|
+        parse_item(Nokogiri::XML(item.to_s))
+    end
+end
+
+
+print_feed("http://tokyotosho.info/rss.php?filter=1")
+print_feed("http://tokyotosho.info/rss.php?filter=8")
